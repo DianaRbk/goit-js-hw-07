9dc7ca9e44d1de9be1083d01a21669ab97304aa0
DianaRbk <robakdiana@gmail.com>
2023-05-17 18:24:45 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function generateGalleryItemMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `;
+  }).join("");
+}
+
+function openModal(imageUrl) {
+  const modal = basicLightbox.create(`<img src="${imageUrl}">`);
+  modal.show();
+
+  window.addEventListener("keydown", closeModalOnEscape);
+
+  function closeModalOnEscape(e) {
+    if (e.code === "Escape") {
+      modal.close();
+      window.removeEventListener("keydown", closeModalOnEscape);
+    }
+  }
+}
+
+function handleGalleryClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imageUrl = e.target.dataset.source;
+  openModal(imageUrl);
+}
+
+const galleryItemMarkup = generateGalleryItemMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+gallery.addEventListener("click", handleGalleryClick);
+
+
+
+
+
+
+
+
+
+
+
+
